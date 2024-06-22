7957d6ebb8451cdf8c543e86728de58d0745d4fe
p1d0ras1337 <ibull3009@gmail.com>
2024-06-22 12:12:11 +0300

    Add files via upload

---
+++ output/networksystem.dll.cs
@@ -0,0 +1,15 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2024-06-22 09:07:33.440491400 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: networksystem.dll
+    // Classes count: 1
+    // Enums count: 0
+    public static class NetworksystemDll {
+        // Parent: None
+        // Fields count: 1
+        public static class ChangeAccessorFieldPathIndex_t {
+            public const nint m_Value = 0x0; // int16
+        }
+    }
+}
---
+++ output/networksystem.dll.rs
@@ -0,0 +1,19 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2024-06-22 09:07:33.440491400 UTC
+
+#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
+
+pub mod cs2_dumper {
+    pub mod schemas {
+        // Module: networksystem.dll
+        // Classes count: 1
+        // Enums count: 0
+        pub mod networksystem_dll {
+            // Parent: None
+            // Fields count: 1
+            pub mod ChangeAccessorFieldPathIndex_t {
+                pub const m_Value: usize = 0x0; // int16
+            }
+        }
+    }
+}
